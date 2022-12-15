6667d3eda1eaceb503b84e403c806f3ac3e9f63b
yashwanth-yasp <100475760+yashwanth-yasp@users.noreply.github.com>
2022-12-15 08:36:23 +0000

    Update basic_pdf_script.py

--- basic_pdf_script.py
+++ basic_pdf_script.py
@@ -16,22 +16,3 @@ with open('Text.pdf', 'rb') as f :
             topics.append(data)
 for i in range(1, len(topics)):
     print(f"{i}. {topics[i]}")
-
-
-# #%%
-# import PyPDF2
-# topics = []
-# with open('Text.pdf', 'rb') as f :
-#     pdf = PyPDF2.PdfFileReader(f)
-#     pageObj = pdf.getPage(0)
-#     data = pageObj.extract_text(0)
-#     dots = ""
-#     for i in data:
-#         if i!= '\n':
-#             dots += i
-#         else:
-#             break
-    
-# print(dots)
-
-## %%
